4744cbf46f2145198824d33f913886ed16703f71
Carlos Eduardo <cegalvao13@gmail.com>
2021-11-12 00:05:00 -0300

    heroku string

--- models/conn/index.js
+++ models/conn/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 async function Conn(){
-    await mongoose.connect("mongodb+srv://cegalvao:eduardo1992@cluster0.qjnqk.mongodb.net/Projeto?retryWrites=true&w=majority").then(() => {
+    await mongoose.connect(process.env.MONGOURI).then(() => {
         console.log("MongoDB esta conectado");
     }).catch((err) => {
         console.error(err);

edde48c13e00413346e5a28d09a4e48b823d35a8
Carlos Eduardo <cegalvao13@gmail.com>
2021-11-12 00:08:17 -0300

    mudar porta

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const Conn = require("./models/conn/index");
 Conn();
 
 
-const port = 3000; 
-
 
 const cidadesRouter = require("./routers/cidades.routes");
 
@@ -18,8 +16,8 @@ app.use('/cidades',cidadesRouter);
 app.use('/estados',estadosRouter);
 app.use('/paises',paisesRouter);
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em: http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
 });
 
 

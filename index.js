3a03fd516e90796792f0fef9bef8f00033b95ba0
Carlos Eduardo <cegalvao13@gmail.com>
2021-11-11 21:59:45 -0300

    validando rotas

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+app.use(express.json()); 
+const Conn = require("./models/conn/index");
+Conn();
+
+
+const port = 3000; 
+
+
+const cidadesRouter = require("./routers/cidades.routes");
+
+const estadosRouter = require("./routers/estados.routes");
+
+const paisesRouter = require("./routers/paises.routes");
+
+app.use('/cidades',cidadesRouter);
+app.use('/estados',estadosRouter);
+app.use('/paises',paisesRouter);
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+});
---
+++ models/cidades.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const cidadesModel = new mongoose.Schema({
+    nome: { type: String, required: true },
+    quantidadeDeBairros: { type: Number, required: true },
+    populacao: { type: Number, required: true },
+    dataAniversarioCidade: { type: Date, required: true }
+});
+
+const Cidade = mongoose.model("Cidades",cidadesModel);
+
+module.exports = Cidade;
